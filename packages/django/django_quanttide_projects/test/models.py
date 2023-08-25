c73ed7240f511261b9740a1b92f37d23dc519e44
Guo Zhang <zhangguo@quanttide.com>
2023-08-25 16:01:40 +0800

    (django) feat: 增加MatterModelTestCase

--- packages/django/django_quanttide_projects/test/models.py
+++ packages/django/django_quanttide_projects/test/models.py
@@ -1,6 +1,6 @@
 from django_quanttide.test import ModelTestCase
 
-from django_quanttide_projects.models import BaseProject
+from django_quanttide_projects.models import BaseProject, BaseMatter
 
 
 class ProjectModelTestCase(ModelTestCase):
@@ -17,3 +17,22 @@ class ProjectModelTestCase(ModelTestCase):
         previous_updated_at = model_instance.updated_at
         model_instance.save()
         self.assertGreater(model_instance.updated_at, previous_updated_at)
+
+
+class MatterModelTestCase(ModelTestCase):
+    model_class: BaseMatter = BaseMatter
+    model_data: dict = {}
+
+    def test_defaults(self):
+        model_instance: BaseProject = super().test_defaults()
+        # Test created_at field
+        self.assertIsNotNone(model_instance.created_at)
+        # Test updated_at field
+        previous_updated_at = model_instance.updated_at
+        model_instance.save()
+        self.assertGreater(model_instance.updated_at, previous_updated_at)
+        # Test title field
+        self.assertEqual(model_instance.title, self.model_data['title'])
+        # Test description field
+        self.assertEqual(model_instance.description, self.model_data['description'])
+
